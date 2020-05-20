e6b8bf1e15bd1d665a0adf069c9c91ede899fa6d
timenbob <timen.bobnar@gmail.com>
2020-05-20 12:25:43 +0200

    delujoče vislice

--- model.py
+++ model.py
@@ -1,24 +1,27 @@
 import random
 
-STEVILO_DOVOLJENIH_NAPAK = 10
+STEVILO_DOVOLJENIH_NAPAK = 9
 PRAVILNA_CRKA = "+"
-PONOVLJENA_CRKA = "0"
+PONOVLJENA_CRKA = "O"
+VEC_KOT_CRKA = ">"
+POSEBEN_ZNAK = "#"
 NAPACNA_CRKA = "-"
 ZMAGA = "W"
-PORAZ = "L"
+PORAZ = "X"
+
 
 class Igra:
 
-    def __init__(self, geslo, crke = None):
+    def __init__(self, geslo, crke=None):
         self.geslo = geslo
         if crke is None:
             self.crke = []
         else:
             self.crke = crke
-    
+
     def napacne_crke(self):
         return [crka for crka in self.crke if crka not in self.geslo]
-    
+
     def pravilne_crke(self):
         return [crka for crka in self.crke if crka in self.geslo]
 
@@ -26,27 +29,29 @@ class Igra:
         return len(self.napacne_crke())
 
     def zmaga(self):
-        for crka in self.geslo:
-            if not crka in self.crke:
-                return False
-            return True
+        return all(crka in self.crke for crka in self.geslo)
+
     def poraz(self):
         return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
-    
+
     def pravilni_del_gesla(self):
-        delni = ""
+        delni = ''
         for crka in self.geslo:
             if crka in self.crke:
                 delni += crka + " "
             else:
-                delni += "_"
+                delni += '_ '
         return delni[:-1]
 
     def nepravilni_ugibi(self):
         return " ".join(self.napacne_crke())
-            
+
     def ugibaj(self, ugib):
+        if len(ugib) > 1 or len(ugib) == 0:
+            return VEC_KOT_CRKA
         crka = ugib.upper()
+        if crka not in 'ABCČDEFGHIJKLMNOPRSŠTUVZŽXYZWQĆĐ':
+            return POSEBEN_ZNAK
         if crka in self.crke:
             return PONOVLJENA_CRKA
         else:
@@ -62,18 +67,11 @@ class Igra:
                     return PORAZ
                 else:
                     return NAPACNA_CRKA
-            
-with open("C:\\Users\\HP\\Documents\\UVP\\vislice\\besede.txt", "r", encoding="utf8") as datoteka_z_besedami:
+
+
+with open("besede.txt", "r", encoding="utf-8") as datoteka_z_besedami:
     bazen_besed = [vrstica.strip().upper() for vrstica in datoteka_z_besedami]
+
 
 def nova_igra():
     return Igra(random.choice(bazen_besed))
-
-
-        
-
-
-
-igra = Igra("ABRAKADABRA", ["A", "E", "I", "O", "U"])
-
-
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -3,55 +3,78 @@ import model
 
 def izpis_igre(igra):
     tekst = (
-        "======================================================="
-        "Število preostalih poskusov: {stevilo_preostalih_poskusov} \n\n"
-        "       {pravilni_del_gesla}"
-        "Neuspeli poskusi: {neuspeli_poskusi}\n\n"
-        "======================================================="
+        '================================================\n\n'
+        'Število preostalih poskusov: {stevilo_preostalih_poskusov} \n\n'
+        '       {pravilni_del_gesla}\n\n'
+        'Neuspeli poskusi: {neuspeli_poskusi}\n\n'
+        '================================================'
     ).format(
-        stevilo_preostali_poskusov=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak() + 1,
+        stevilo_preostalih_poskusov=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak() + 1,
         pravilni_del_gesla=igra.pravilni_del_gesla(),
         neuspeli_poskusi=igra.nepravilni_ugibi()
     )
     return tekst
 
+
 def izpis_zmage(igra):
     tekst = (
-        "\n######Ideee, zmaga stari! Geslo je bilo: {geslo} ######\n\n"
-
-    ).format(
-        geslo=igra.geslo()
-    )
-    return tekst
-
-def izpis_poraza(igra):
-    tekst = (
-        "\n######## Jadnik, zgubu si! Geslo je bilo: {geslo} ######\n\n"
+        '\n######## Wipiiiii, zmaga! Geslo je bilo: {geslo} ########\n\n'
     ).format(
         geslo=igra.geslo
     )
     return tekst
 
+
+def izpis_poraza(igra):
+    tekst = (
+        '\n######## Boooo, poraz! Geslo je bilo: {geslo} ########\n\n'
+    ).format(
+        geslo=igra.geslo
+    )
+    return tekst
+
+
+def izpis_napake():
+    return '\n######## Ugiba se ena črka naenkrat! ########\n\n'
+
+
+def izpis_napake_znak():
+    return '\n######## Ugib naj ne vsebuje posebnih znakov! ########\n\n'
+
+
 def zahtevaj_vnos():
-    return input("Črka: ")
+    return input('Črka: ')
 
 
 def pozeni_vmesnik():
-    igra = model.nova_igra()
-    
-    while True:
-        #najprej izpišemo stanje. da vidimo koliko črk je
-        print(izpis_igre(igra))
-        #čakamo na črko od uporabnika
-        poskus = zahtevaj_vnos()
-        igra.ugibaj(poskus)
-        if igra.zmaga():
-            print(izpis_zmage(igra))
-            break
-        elif igra.poraz():
-            print(izpis_igre.poraza())
-            break
-    return
 
-#zaženi igro:
+    igra = model.nova_igra()
+
+    while True:
+        # najprej izpisemo stanje, da vidimo, koliko črk je ipd.
+        print(izpis_igre(igra))
+        # čakamo na črko od uporabnika
+        poskus = zahtevaj_vnos()
+        rezultat_ugiba = igra.ugibaj(poskus)
+        if rezultat_ugiba == model.VEC_KOT_CRKA:
+            print(izpis_napake())
+        elif rezultat_ugiba == model.POSEBEN_ZNAK:
+            print(izpis_napake_znak())
+        elif rezultat_ugiba == model.ZMAGA:
+            print(izpis_zmage(igra))
+            ponvni_zagon = input("Za ponovni zagon vpišite 1.").strip()
+            if ponvni_zagon == "1":
+                igra = model.nova_igra()
+            else:
+                break
+        elif rezultat_ugiba == model.PORAZ:
+            print(izpis_poraza(igra))
+            ponvni_zagon = input("Za ponovni zagon vpišite 1.\n").strip()
+            if ponvni_zagon == "1":
+                igra = model.nova_igra()
+            else:
+                break
+
+
+# Zaženi igro:
 pozeni_vmesnik()
